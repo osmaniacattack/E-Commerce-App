978a9d0676402b61cdaf6d684072e3d8a075e3d8
abdul-osman-mecca <39681296+osmaniacattack@users.noreply.github.com>
2022-08-15 10:46:57 -0700

    configure react router

--- emart/src/App.js
+++ emart/src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import Navbar from './component/Navbar';
-import Home from './component/Home';
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import Products from "./component/Products"
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Home/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
+      </Routes>
     </>
   );
 }
