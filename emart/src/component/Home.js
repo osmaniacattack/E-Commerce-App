e007e7d2667f2f51ac91b7866d00ca8014b2b6fc
abdul-osman-mecca <39681296+osmaniacattack@users.noreply.github.com>
2022-08-15 10:22:39 -0700

    Implement product page with fake product api

--- emart/src/component/Home.js
+++ emart/src/component/Home.js
@@ -1,24 +1,26 @@
 import React from "react";
+import Products from "./Products";
 
 export default function Home() {
   return (
     <div className="hero">
-      <div class="card bg-dark text-white border-0">
+      <div className="card bg-dark text-white border-0">
         <img
           src="https://picsum.photos/200/300/?blur"
-          class="card-img"
+          className="card-img"
           height="550px"
           alt="Background"
         />
-        <div class="card-img-overlay d-flex flex-column justify-content-center">
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
           <div className="container">
-            <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-            <p class="card-text lead fs-2">
+            <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+            <p className="card-text lead fs-2">
               CHECK OUT ALL THE TRENDS
             </p>
           </div>
         </div>
       </div>
+      <Products/>
     </div>
   );
 }
